e8ee29231ee85c880b27181bb098cec7d71a6609
Константин <k336699k@mail.ru>
2015-07-29 21:43:10 +0300

    second commit

---
+++ lab1/HelloWorldNew.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("переход на новую ветку dev");
+    }
+}
+
